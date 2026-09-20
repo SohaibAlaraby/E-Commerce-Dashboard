387d56b587c3cba0fa9935655cbca78d1ae5e4a6
SohaibAlaraby <sohibalaraby9@gmail.com>
2026-09-20 23:51:17 +0300

    feat(sidebar): build the tsx sturcture of the sidebar

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -1,9 +1,26 @@
-export default function Sidebar(){
+import { SidebarData } from "../../data/sidebarData";
+import NavLink from "./NavLink";
+
+export default function Sidebar({title, appIcon, items}:SidebarData){
     return (
         <aside>
             <header>
-                <h1>SalesVista</h1>
+                <img src={appIcon} alt="pie chart"/>
+                <h1 >{title}</h1>
             </header>
+            <ul>
+                {items.map((item) => {
+                    const Icon = item.icon;
+                    return(
+                        <li>
+                            <NavLink href={item.href} className="flex">
+                                <Icon/>
+                                {item.name}
+                            </NavLink>
+                        </li>
+                    );
+                })}
+            </ul>
         </aside>
     );
 }
