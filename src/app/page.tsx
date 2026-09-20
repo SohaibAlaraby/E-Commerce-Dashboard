08bf964f4ef36e274f1b546c3c45409639381651
SohaibAlaraby <sohibalaraby9@gmail.com>
2026-09-20 15:24:14 +0300

    chore(files): modify files structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {mockOrdersData} from "../src/data/mockData/mockOrders"
+import {mockOrdersData} from "../data/mockData/mockOrders"
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
